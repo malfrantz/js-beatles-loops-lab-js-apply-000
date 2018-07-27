4cd75b2d435961046ad0c732f918908ffad1502f
Mallory Frantz <mallory.rosenberger@gmail.com>
2018-07-27 02:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArr = [];
+  for (var i = 0; i < 4; i++) {
+      myArr.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return myArr;
+}
+
+theBeatlesPlay(musicians, instruments);
+
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
