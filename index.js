a2c12d9d4cc4bff4d6036fb3a4834499b86bbe96
Mallory Frantz <mallory.rosenberger@gmail.com>
2018-07-27 02:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 theBeatlesPlay(musicians, instruments);
-    
+  
+function johnLennonFacts(){
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
    var newFacts = [];
-    var i = 0;
+   var i = 0;
    while (i < facts.length) {
         newFacts.push(facts[i] +"!!!");
         i++;
     }
     return newFacts;
 }
+
 function iLoveTheBeatles(x) {
   var array = [];
   do {
